6dbdf9a7b0f168113212c70fa3a1409a1306f801
DAN3002 <dinhanh300229@gmail.com>
2025-03-03 18:41:12 +0700

    ref: convert indent to tab

--- utils/logger.py
+++ utils/logger.py
@@ -3,27 +3,27 @@ import os
 from datetime import datetime
 
 def setup_logger(name, log_file):
-    """Set up logger with file and console handlers"""
-    # Create logs directory if it doesn't exist
-    log_dir = os.path.dirname(log_file)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    
-    # Create logger
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    
-    # Create handlers
-    file_handler = logging.FileHandler(log_file, encoding='utf-8')
-    console_handler = logging.StreamHandler()
-    
-    # Create formatters and add it to handlers
-    log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(log_format)
-    console_handler.setFormatter(log_format)
-    
-    # Add handlers to the logger
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-    
-    return logger 
+	"""Set up logger with file and console handlers"""
+	# Create logs directory if it doesn't exist
+	log_dir = os.path.dirname(log_file)
+	if not os.path.exists(log_dir):
+		os.makedirs(log_dir)
+	
+	# Create logger
+	logger = logging.getLogger(name)
+	logger.setLevel(logging.INFO)
+	
+	# Create handlers
+	file_handler = logging.FileHandler(log_file, encoding='utf-8')
+	console_handler = logging.StreamHandler()
+	
+	# Create formatters and add it to handlers
+	log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	file_handler.setFormatter(log_format)
+	console_handler.setFormatter(log_format)
+	
+	# Add handlers to the logger
+	logger.addHandler(file_handler)
+	logger.addHandler(console_handler)
+	
+	return logger 
